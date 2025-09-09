dcb61f1d464f62dfd5cbfe4e11a3a131e1445945
Martin Murithi <mmurithi18@gmail.com>
2025-09-09 12:47:30 +0300

    updated commit message

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./*.html",              // Add this line
-    "./index.html"           // Add this line if you have index.html in root
-  ],
+  content: ["./src/**/*.{html,js}", "./*.html"], // Adjust paths to match your structure
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
